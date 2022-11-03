662869b8a97005cfcdbf331b6eb4b4282971f9e9
Nishat Taaha <73392394+NishatTaaha@users.noreply.github.com>
2022-11-04 01:55:13 +0600

    Delete LinkedList Python directory

--- LinkedList Python/test.py
+++
@@ -1,9 +0,0 @@
-from LinkedListPackage import LinkedList
-PyLL=LinkedList()
-PyLL.insert_begin(10)
-PyLL.insert_begin(20)
-PyLL.insert_begin(30)
-PyLL.display()
-print(PyLL.find_index(10))
-print(PyLL.elemCount())
-PyLL.display()
